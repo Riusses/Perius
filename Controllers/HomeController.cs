1e7b2550a5f7e086ea2bef98afd3013f5903891f
Riusses <rius.45@gmail.com>
2022-09-11 19:01:47 +0200

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Perius.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace Perius.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+
+            try
+            {
+                string queryString = "SELECT IdCliente, Nombre FROM dbo.Clientes;";
+                using SqlConnection connection = new("Server=localhost;Database=periusdb;Trusted_Connection=True;");
+                SqlCommand command = new(queryString, connection);
+                connection.Open();
+                using SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    ViewBag.nombre = string.Format("{0}, {1}", reader[0], reader[1]);
+                }
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
